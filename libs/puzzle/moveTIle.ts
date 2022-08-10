148c4abfe35fb6d22b4ffb2a03c09d86eb72d2f0
kyk1211 <85268135+kyk1211@users.noreply.github.com>
2022-08-10 21:32:40 +0900

    Update moveTIle.ts

--- libs/puzzle/moveTIle.ts
+++ libs/puzzle/moveTIle.ts
@@ -1,24 +1,3 @@
-const moveTile = (config: Config) => {
-  config.tiles.forEach((tile, index) => {
-    tile.onMouseDown = (event: any) => {
-      console.log('down', event);
-    };
-    tile.onMouseDrag = (event: any) => {
-      console.log('drag', event);
-      tile.position.x += event.delta.x;
-      tile.position.y += event.delta.y;
-    };
-    tile.onMouseUp = (event: any) => {
-      console.log('up', event);
-    };
-  });
-};
-
-const puzzle = {
-  moveTile,
-};
-
-export default puzzle;
 import { getMargin } from './createPuzzle';
 
 const moveTile = (config: Config) => {
