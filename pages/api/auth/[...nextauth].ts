38f57e8f193bdebbec5337d598e9afe766096cc0
곽윤구 <dbsrndi123@naver.com>
2022-09-11 21:18:01 +0900

    feat: session user add provider property

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -18,4 +18,18 @@ export default NextAuth({
       clientSecret: process.env.NAVER_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.provider = account.provider;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.user.provider = token.provider;
+      return session;
+    },
+  },
 });
---
+++ types/next-auth.d.ts
@@ -0,0 +1,16 @@
+import NextAuth, { DefaultSession } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      provider: string | undefined;
+    } & DefaultSession['user'];
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    provider?: string;
+  }
+}
