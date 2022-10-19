33a9a2f07c938e13121046b81a9d69d690c78a22
catca <kdg96g@gmail.com>
2022-10-19 23:47:10 +0900

    fix: 소켓 통신 주소 수정

--- libs/context/socket.tsx
+++ libs/context/socket.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as io from 'socket.io-client';
-export const socket = io.connect('http://localhost:3000/', {
+export const socket = io.connect({
   path: '/api/socketio',
 });
 export const SocketContext = React.createContext(socket);
