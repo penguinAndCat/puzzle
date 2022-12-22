6aaad8fb820ed5963dc7f1176ce0c156f22f6baf
catca <kdg96g@gmail.com>
2022-12-22 10:53:00 +0900

    feat: 테마 적용 시 깜빡임 현상 수정
    테마 저장 위치 localstorage => cookie 변경
    getInitialProps 사용하여 테마 ssr 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import Toast from 'components/common/Toast';
 import Loading from 'components/common/Loading';
 import Modal from 'components/common/Modal';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { getCookie } from 'cookies-next';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,7 +20,7 @@ const queryClient = new QueryClient({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
+      <ThemeProvider pageTheme={pageProps.theme}>
         <GlobalStyle />
         <Component {...pageProps} />
         <Toast />
@@ -29,5 +30,29 @@ function MyApp({ Component, pageProps }: AppProps) {
     </QueryClientProvider>
   );
 }
+
+MyApp.getInitialProps = async ({ ctx, Component }: {ctx: any; Component: any;}) => {
+  const themeKeys: ThemeKey[] = ['pink', 'silver', 'mint', 'dark'];
+  let pageProps: any = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  const cookie = ctx.req.cookies;
+  if (cookie) {
+    const localTheme = getCookie('localTheme', ctx) as ThemeKey;
+    if (themeKeys.includes(localTheme)) {
+      Object.assign(pageProps, {
+        theme: localTheme,
+      });
+    } else {
+      Object.assign(pageProps, {
+        theme: 'pink',
+      });
+    }
+  }
+
+  return { pageProps };
+};
 
 export default MyApp;
