9326dd64cdb34c960a34ba9ac44a3aca5f8bc0c6
catca <kdg96g@gmail.com>
2023-03-07 23:54:28 +0900

    docs: storybook 소개 작성

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-    '../components/common/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../components/common/**/*.stories.mdx', '../components/common/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
